69e6b34e44b5b527c94d0f98a23ab14f866c2b46
prato <prato@ARUN_SKS>
2025-02-07 00:54:37 +0530

    add

--- src/test/java/com/sampleprogram/pages/ArunProgram.java
+++ src/test/java/com/sampleprogram/pages/ArunProgram.java
@@ -1,10 +1,16 @@
 package com.sampleprogram.pages;
 
+import java.util.Scanner;
+
 public class ArunProgram {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("my name is arun");
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		i = i + 5;
+		System.out.println(i);
 	}
 
 }
