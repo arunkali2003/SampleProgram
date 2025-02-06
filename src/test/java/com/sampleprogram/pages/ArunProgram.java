240623b495a03f537781f3bfe64f11744cda6a9e
prato <prato@ARUN_SKS>
2025-02-07 00:46:46 +0530

    my name is arun

---
+++ src/test/java/com/sampleprogram/pages/ArunProgram.java
@@ -0,0 +1,10 @@
+package com.sampleprogram.pages;
+
+public class ArunProgram {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("my name is arun");
+	}
+
+}
