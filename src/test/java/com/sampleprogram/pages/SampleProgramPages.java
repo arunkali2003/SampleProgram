61874763293979d6320c8e0709dc03e177f0d86e
prato <prato@ARUN_SKS>
2025-02-07 00:42:34 +0530

    my first program

---
+++ src/test/java/com/sampleprogram/pages/SampleProgramPages.java
@@ -0,0 +1,10 @@
+package com.sampleprogram.pages;
+
+public class SampleProgramPages {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("My first project");
+	}
+
+}
